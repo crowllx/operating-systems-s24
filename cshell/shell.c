f3ba0a8a01c224528a94dc5754e17747064d2210
crowllx <114693487+crowllx@users.noreply.github.com>
2024-05-09 18:30:00 -0700

    update

--- cshell/shell.c
+++ cshell/shell.c
@@ -38,25 +38,22 @@ enum Operator check_ops(char *token) {
 int execute_cmd(char *argv[], enum Operator flag, int *fd, char *fp) {
 
     int status = 0;
-    printf("cmd: %s\narg: %s\n", argv[0], argv[1]);
+    printf("cmd: %s\narg: %s\n op: %d", argv[0], argv[1], flag);
+    printf("%p\n", argv);
     pid_t pid = fork();
     if (pid < 0) {
         perror("Failed to fork\n");
     } else if (pid == 0) {
         if (flag == PIPE) {
-            close(fd[0]);
             dup2(fd[1], 1);
-            close(fd[1]);
-        } else {
-            close(fd[1]);
-            dup2(fd[0], 0);
-            close(fd[0]);
         }
+        close(fd[1]);
+        dup2(fd[0], 0);
+        close(fd[0]);
         if (execvp(argv[0], argv) < 0) {
             printf("\nFailed to execute command.\n");
             exit(-1);
         }
-        close(0);
         fflush(stdout);
         exit(0);
     } else {
@@ -86,14 +83,15 @@ int main(int argc, char *argv[]) {
             return -1;
         }
         while (token_idx < tok_count) {
+            printf("token: %s\n", tokens[token_idx]);
             op = check_ops(tokens[token_idx]);
             if (op != STDOUT && token_idx < tok_count - 1) {
                 tokens[token_idx] = NULL;
                 file_path = tokens[token_idx + 1];
                 status = execute_cmd(cmd, op, fd, file_path);
                 printf("%d\n", status);
-                token_idx++;
-                cmd += token_idx;
+                printf("%s\n", tokens[token_idx + 1]);
+                cmd = &tokens[token_idx + 1];
             }
             token_idx++;
         }
