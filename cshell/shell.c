4546cc5c1b9830af449af3bbc1fd5d6add487b65
crowllx <114693487+crowllx@users.noreply.github.com>
2024-05-10 09:30:07 -0700

    Squashed commit of the following:
    commit bf1ff978303bbd9fdd81580aceafa125cbdb961e
    Author: crowllx <114693487+crowllx@users.noreply.github.com>
    Date:   Fri May 10 09:28:24 2024 -0700
    
        redirection complete
    
    commit fa1a4e81ff72ce79937027d70d61daa53ed3a28a
    Author: crowllx <114693487+crowllx@users.noreply.github.com>
    Date:   Fri May 10 07:11:11 2024 -0700
    
        add ability to track status of executed commands

--- cshell/shell.c
+++ cshell/shell.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,8 +11,16 @@
 // Operating Systems
 
 #define DELIMS " \t\n\a\r"
-enum Operator { STDOUT, REDIRECT_STDIN, REDIRECT_STDOUT, PIPE };
+#define prompt_color "\x1b[35m"
+#define bold "\x1b[1m"
+#define color_end "\x1b[0m"
 
+enum Operator { NONE, STDOUT, REDIRECT_STDIN, REDIRECT_STDOUT, PIPE };
+
+void prompt() { printf(prompt_color bold "\n> " color_end); }
+
+// break given command in to null terminated lists of strings split on
+// whitespace
 char **tokenize(char *line, size_t *count) {
     char **tokens = malloc(sizeof(char *) * 20);
 
@@ -25,6 +34,7 @@ char **tokenize(char *line, size_t *count) {
     tokens[*count] = NULL;
     return tokens;
 }
+
 enum Operator check_ops(char *token) {
     if (strcmp(token, "|") == 0) {
         return PIPE;
@@ -37,9 +47,11 @@ enum Operator check_ops(char *token) {
     }
 }
 
-void execute_piped(char *argv[], int *prev_pfd) {
+// execute a command and redirect its stdout to a pipe
+int execute_piped(char *argv[], int *prev_pfd) {
     int pfds[2];
-
+    printf("*** PIPED ***\n");
+    int status;
     if (pipe(pfds) < 0) {
         perror("pipe failed");
         exit(-1);
@@ -64,11 +76,55 @@ void execute_piped(char *argv[], int *prev_pfd) {
     } else {
         close(pfds[1]);
         *prev_pfd = pfds[0]; // save read end;
+        wait(&status);
     }
+    return status;
 }
-void execute(char *argv[], int *prev_pfd) {
+
+// TODO: allow redirections that precede pipes
+// execute a command and either redirect a file to its STDIN
+// or redirect its STDOUT to a file
+int execute_redirect(char *argv[], int *prev_pfd, char *fp, enum Operator op) {
+    int status;
+    printf("*** REDIRECT ***\n");
+
     pid_t pid = fork();
-    printf("%d\n", pid);
+    if (pid < 0) {
+        perror("fork failed");
+        exit(-1);
+    } else if (pid == 0) {
+        if (op == REDIRECT_STDOUT) {
+            if (prev_pfd != STDIN_FILENO) {
+                dup2(*prev_pfd, STDIN_FILENO); // previous pipe -> stdin
+                close(*prev_pfd);
+            }
+            int fd = open(fp, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+            if (fd < 0) {
+                perror("error on open: ");
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+        } else if (op == REDIRECT_STDIN) {
+            int fd = open(fp, O_RDONLY);
+            if (fd < 0) {
+                perror("error on open: ");
+            }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+        }
+        execvp(argv[0], argv);
+        perror("error on execvp");
+        exit(-1);
+    } else {
+        wait(&status);
+    }
+    return status;
+}
+
+int execute(char *argv[], int *prev_pfd) {
+    printf("*** EXEC ***\n");
+    int status;
+    pid_t pid = fork();
     if (pid == 0) {
         if (prev_pfd != STDIN_FILENO) {
             dup2(*prev_pfd, STDIN_FILENO); // previous pipe -> stdin
@@ -78,14 +134,16 @@ void execute(char *argv[], int *prev_pfd) {
         perror("execvp err");
         exit(0);
     } else {
-        wait(NULL);
+        wait(&status);
     }
+    return status;
 }
 
 int main(int argc, char *argv[]) {
     char buf[80];
 
     while (1) {
+        prompt();
         fgets(buf, 80, stdin);
         char *line = buf;
         size_t tok_count = 0;
@@ -93,29 +151,42 @@ int main(int argc, char *argv[]) {
         enum Operator op = STDOUT;
         size_t token_idx = 0;
         int prev_pfd, pfd[2];
-        char **cmds[20];
-        cmds[0] = tokens;
-        int cmds_count = 1;
+        size_t next_cmd = 0;
+        int prev_status = 0;
 
-        // break commands in to null terminated lists of arguments
         while (token_idx < tok_count) {
             printf("token: %s\n", tokens[token_idx]);
             op = check_ops(tokens[token_idx]);
-            if (op != STDOUT) {
+            switch (op) {
+            case PIPE:
                 tokens[token_idx] = NULL;
-                cmds[cmds_count] = &tokens[token_idx + 1];
-                cmds_count++;
+                printf("tokens: %p, cmd: %p, op: %d\n", tokens,
+                       tokens[next_cmd], op);
+                printf("cmd: %s arg1: %s\n", tokens[next_cmd],
+                       tokens[next_cmd + 1]);
+                prev_status = execute_piped(&tokens[next_cmd], &prev_pfd);
+                next_cmd = token_idx + 1;
+                break;
+            case REDIRECT_STDIN:
+            case REDIRECT_STDOUT:
+                tokens[token_idx] = NULL;
+                char *fp = tokens[token_idx + 1];
+                prev_status =
+                    execute_redirect(&tokens[next_cmd], &prev_pfd, fp, op);
+                next_cmd = token_idx + 2;
+                token_idx++;
+                break;
+            default:
+                break;
             }
             token_idx++;
         }
-
-        // execute commands
-        size_t i = 0;
-        while (i < cmds_count - 1) {
-            execute_piped(cmds[i], &prev_pfd);
-            i++;
+        // execute last command
+        printf("tokens: %p, cmd: %p\n", tokens, tokens[next_cmd]);
+        printf("cmd: %s arg1: %s\n", tokens[next_cmd], tokens[next_cmd + 1]);
+        if (tokens[next_cmd] != NULL) {
+            prev_status = execute(&tokens[next_cmd], &prev_pfd);
         }
-        execute(cmds[cmds_count - 1], &prev_pfd);
         if (prev_pfd != STDIN_FILENO) {
             printf("PREV closed\n");
             close(prev_pfd);
