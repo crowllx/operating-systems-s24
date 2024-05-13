fe462fcf3f70cebd250dcdc9c62089fdb716bd66
crowllx <114693487+crowllx@users.noreply.github.com>
2024-05-12 20:51:47 -0700

    fix execute loop and improve chdir usage

--- cshell/shell.c
+++ cshell/shell.c
@@ -48,14 +48,19 @@ void print_cmd(struct Command *cmd) {
 }
 
 void change_dir(struct Command *cmd) {
-    print_cmd(cmd);
-    char *path = cmd->args[1];
-    printf("path: %s\n", path);
+    /* print_cmd(cmd); */
+    char *path;
+    if (cmd->args[1] == NULL || strcmp(cmd->args[1], "~") == 0) {
+        path = getenv("HOME");
+    } else {
+        path = cmd->args[1];
+    }
+
     if (chdir(path) < 0) {
-        perror("chdir err: ");
-        exit(0);
+        perror("cd");
     }
 }
+
 char **build_args() {
     char **args = malloc(sizeof(char *) * MAX_TOKENS);
     for (size_t i = 0; i < MAX_TOKENS; i++) {
@@ -63,6 +68,7 @@ char **build_args() {
     }
     return args;
 }
+
 void parse_commands(char *line, size_t *count, struct Command *cmds) {
     char *tok = strtok(line, DELIMS);
     while (*count < MAX_CMDS && tok != NULL) {
@@ -102,21 +108,22 @@ void parse_commands(char *line, size_t *count, struct Command *cmds) {
     }
 }
 void exec_cmd(struct Command *cmd, int *prev_pfd, int pfds[2]) {
-    if (strcmp(cmd->args[0], "cd") == 0) {
-        // TODO figure out why this causes infinite loop
-        change_dir(cmd);
-        return;
-    }
-    int retval = pipe(pfds);
-    if (retval < 0) {
-        perror("pipe error");
+
+    int rval = pipe(pfds);
+    if (rval < 0) {
+        perror("pipe");
         exit(0);
     }
     /* printf("new pipe fds: %d, %d\n\n", pfds[0], pfds[1]); */
 
+    if (strcmp(cmd->args[0], "cd") == 0) {
+        change_dir(cmd);
+        return;
+    }
+
     pid_t pid = fork();
     if (pid < 0) {
-        perror("fork failed");
+        perror("fork");
         exit(0);
     } else if (pid == 0) {
         // redirect STDIN, either from file or from previous commands pipe
@@ -146,7 +153,7 @@ void exec_cmd(struct Command *cmd, int *prev_pfd, int pfds[2]) {
 
         // execute cmd
         execvp(cmd->args[0], cmd->args);
-        perror("exec err: ");
+        perror(cmd->args[0]);
         exit(0);
     } else {
         cmd->pid = pid;
@@ -164,19 +171,21 @@ int main(int argc, char *argv[]) {
         size_t cmd_count = 0;
         int exit_flag = -1;
         struct Command cmds[MAX_CMDS];
+
         // needs free
         parse_commands(line, &cmd_count, cmds);
 
         // debug
-        for (int i = 0; i < cmd_count; i++) {
-            print_cmd(&cmds[i]);
-        }
+        /* for (int i = 0; i < cmd_count; i++) { */
+        /*     print_cmd(&cmds[i]); */
+        /* } */
 
         // execute loop
         int prev_pfd = -1;
         int pfds[cmd_count][2];
         for (size_t i = 0; i < cmd_count; i++) {
-            pfds[i][0] = pfds[i][1] = 0;
+            pfds[cmd_count][0] = 0;
+            pfds[cmd_count][1] = 0;
             cmds[i].read_pfd = prev_pfd;
             if (strcmp(cmds[i].args[0], "exit") == 0) {
                 exit_flag = 0;
